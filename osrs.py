515271d1c8121803d32497fb9321ac0c68a8383b
Coffee-fueled-deadlines <47742787+Coffee-fueled-deadlines@users.noreply.github.com>
2019-07-31 20:45:26 -0400

    Update osrs.py
    Adjusted skill() method to allow for easier access to rank, level, and experience with out having to touch stat dictionary

--- osrs.py
+++ osrs.py
@@ -113,9 +113,13 @@ class Hiscores(object):
 		# set stats dictionary
 		self.stats = subset
 
-	def skill(self,skill):
+	def skill(self, skill, stype):
 		try:
-			return self.stats[skill.lower()]['level']
+			if stype not in ['rank','level','experience']:
+				raise "stype must be 'rank','level', or experience'"
+				exit(0)
+			else:
+				return self.stats[skill.lower()][stype]
 		except KeyError as KE:
 			print("ERROR: skill {} does not exist".format(KE))
 			exit(0)
